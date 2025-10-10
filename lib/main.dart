de4c5ffbf5ce6f5043aaec27bcf5c172964838ce
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-10-10 10:32:01 +0100

    Add const to center

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Welcome to my Sandwich Shop!'),
+            Text('Welcome to my Sandwich Shop!'),
           ],
         ),
       ),
