a4ac75cb249679d3a412d00d76e067791b5da4c4
WoodhouseA <aaronjameswoodhouse@gmail.com>
2025-10-16 19:03:11 +0100

    Add SandwhichContainer class and order in a column

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,38 @@ class App extends StatelessWidget {
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: Center(
-          child: Container(
-            alignment: Alignment.center,
-            width: 300,
-            height: 100,
-            color: Colors.blue,
-            child: const OrderItemDisplay(5, 'Footlong')),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // centers vertically
+            crossAxisAlignment: CrossAxisAlignment.center, // centers horizontally
+            children: [
+              SandwhichContainer(5, 'Footlong'),
+              SizedBox(height: 20),
+              SandwhichContainer(2, 'Bacon'),
+              SizedBox(height: 20),
+              SandwhichContainer(4, 'Club'),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class SandwhichContainer extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const SandwhichContainer(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 300,
+      height: 100,
+      color: Colors.blue,
+      child: OrderItemDisplay(quantity, itemType),
     );
   }
 }
